0230f946ded40ff5a0dfe6a6a483a798e93c426b
iamfarazbaig <iamfarazbaig@gmail.com>
2018-12-22 22:28:04 +0000

    aasd

---
+++ src/main/java/controllers/RestMain.java
@@ -0,0 +1,69 @@
+package controllers;
+
+import io.javalin.Javalin;
+
+public class RestMain {
+
+	public static void main(String[] args) throws Exception {
+		Javalin app = Javalin.create();
+		app.port(getAssignedPort());
+		app.start();
+		PacemakerRestService service = new PacemakerRestService();
+		configRoutes(app, service);
+	}
+
+	static void configRoutes(Javalin app, PacemakerRestService service) {
+
+		app.get("/users", ctx -> {
+			service.listUsers(ctx);
+		});
+
+		app.post("/users", ctx -> {
+			service.createUser(ctx);
+		});
+
+		app.get("/users/:id", ctx -> {
+			service.listUser(ctx);
+		});
+
+		app.get("/users/:id/activities", ctx -> {
+			service.getActivities(ctx);
+		});
+
+		app.post("/users/:id/activities", ctx -> {
+			service.createActivity(ctx);
+		});
+
+		app.get("/users/:id/activities/:activityid", ctx -> {
+			service.getActivity(ctx);
+		});
+
+		app.get("/users/:id/activities/:activityid/locations", ctx -> {
+			service.getActivityLocations(ctx);
+		});
+
+		app.post("/users/:id/activities/:activityid/locations", ctx -> {
+			service.addLocation(ctx);
+		});
+
+		app.delete("/users", ctx -> {
+			service.deleteUsers(ctx);
+		});
+
+		app.delete("/users/:id", ctx -> {
+			service.deleteUser(ctx);
+		});
+
+		app.delete("/users/:id/activities", ctx -> {
+			service.deleteActivities(ctx);
+		});
+	}
+
+	private static int getAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 7500;
+	}
+}
